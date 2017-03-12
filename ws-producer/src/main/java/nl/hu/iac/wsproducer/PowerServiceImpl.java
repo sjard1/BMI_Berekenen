2725d4bd8a05ee2da48b4d1dcfc52d4fc9f8269a
sjard <sjard@msn.com>
2017-03-12 20:57:20 +0100

    lets gooooo

--- ws-producer/src/main/java/nl/hu/iac/wsproducer/PowerServiceImpl.java
+++ ws-producer/src/main/java/nl/hu/iac/wsproducer/PowerServiceImpl.java
@@ -1,27 +1,29 @@
 package nl.hu.iac.wsproducer;
 
+import BMIMeter.Fault;
+import BMIMeter.Request;
+import BMIMeter.Response;
+import BMIMeter.WSInterface;
 import com.sun.xml.ws.developer.SchemaValidation;
-import wsinterface.*;
+import BMIMeter.*;
 
 import javax.jws.WebService;
-import java.math.BigInteger;
 
 
-@WebService( endpointInterface= "wsinterface.WSInterface")
+@WebService( endpointInterface= "BMIMeter.WSInterface")
 @SchemaValidation(handler = SchemaValidationErrorHandler.class)
 
-public class PowerServiceImpl implements wsinterface.WSInterface  {
-
+public class PowerServiceImpl implements WSInterface {
 	@Override
-	public Response calculatePower(Request request) throws Fault{
-		System.out.println("Request object "+request.getX()+ " " +request.getPower());
+	public Response calculatePower(Request request) throws Fault {
+		System.out.println("Request object " + request.getX() + " " + request.getPower());
 		ObjectFactory factory = new ObjectFactory();
 		Response response = factory.createResponse();
 		try {
 			//BMI berekenen
-			BigInteger result = request.getPower();
+			double result = request.getPower();
 			response.setResult(result);
-		} catch (Exception e) {//RuntimeException |
+		} catch (Exception e) {
 			PowerFault x = factory.createPowerFault();
 			x.setErrorCode((short) 1);
 			x.setMessage("error");
@@ -33,6 +35,27 @@ public class PowerServiceImpl implements wsinterface.WSInterface  {
 
 		return response;
 	}
-
-
 }
+
+
+//	@Override
+//	public Response calculatePower(Request request) throws Fault{
+//		System.out.println("Request object "+request.getX()+ " " + request.getPower());
+//		ObjectFactory factory = new ObjectFactory();
+//		Response response = factory.createResponse();
+//		try {
+//			//BMI berekenen
+//			BigInteger result = request.getPower();
+//			response.setResult(result);
+//		} catch (Exception e) {
+//			PowerFault x = factory.createPowerFault();
+//			x.setErrorCode((short) 1);
+//			x.setMessage("error");
+//			Fault fault = new Fault(
+//					"FOUT FOUT FOUT", x);
+//
+//			throw fault;
+//		}
+//
+//		return response;
+//	}
--- ws-producer/src/main/java/wsinterface/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://schemas.hu.fnt.nl/berichten/fault")
-package wsinterface;
